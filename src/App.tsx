d8a1362ac03ab519a62409616cc26c7285b7db08
Volodymyr Semenov <Vandorlot@gmail.com>
2025-04-03 01:39:51 -0400

    Create Game state loop

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,98 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import React, { useReducer } from "react";
+
+type State =
+  | {
+      phase: "pre-game";
+    }
+  | { phase: "in-game"; goal: string; guess: string }
+  | {
+      phase: "post-game";
+      goal: string;
+    };
+
+type Action =
+  | {
+      type: "start-game";
+    }
+  | {
+      type: "update-guess";
+      newGuess: string;
+    };
+
+function getInitialState(): State {
+  return { phase: "pre-game" };
+}
+
+function getRandomWord() {
+  return "Apple";
+}
+
+function reducer(state: State, action: Action): State {
+  switch (action.type) {
+    case "start-game": {
+      if (state.phase === "in-game") {
+        return state;
+      }
+      return { phase: "in-game", goal: getRandomWord(), guess: "" };
+    }
+    case "update-guess": {
+      if (state.phase !== "in-game") {
+        return state;
+      }
+      if (action.newGuess === state.goal) {
+        return {
+          phase: "post-game",
+          goal: state.goal,
+        };
+      }
+      return { ...state, guess: action.newGuess };
+    }
+  }
+  return state;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [state, dispatch] = useReducer(reducer, null, getInitialState);
+
+  switch (state.phase) {
+    case "pre-game": {
+      return (
+        <button onClick={() => dispatch({ type: "start-game" })}>
+          Begin new game
+        </button>
+      );
+    }
+
+    case "in-game": {
+      return (
+        <div>
+          <div>Goal: {state.goal}</div>
+          <label>
+            Guess:
+            <input
+              type="text"
+              value={state.guess}
+              onChange={(ev) =>
+                dispatch({ type: "update-guess", newGuess: ev.target.value })
+              }
+            />
+          </label>
+        </div>
+      );
+    }
+
+    case "post-game": {
+      return (
+        <div>
+          <div>Nice game! You guessed {state.goal}</div>
+          <button onClick={() => dispatch({ type: "start-game" })}>
+            Begin new game
+          </button>
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
