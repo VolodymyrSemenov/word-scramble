b328644f5a929857837718bc328494ead8cf8dab
Volodymyr Semenov <Vandorlot@gmail.com>
2025-04-05 17:28:01 -0400

    Loading wordpack from .txt file over the internet

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import "./App.css";
-import React, { useReducer } from "react";
+import React, { useReducer, useEffect } from "react";
 
-type State =
+type wordPack = readonly string[];
+type State = Readonly<
   | {
       phase: "pre-game";
+      wordpack: wordPack | null;
     }
-  | { phase: "in-game"; goal: string; guess: string }
+  | {
+      phase: "in-game";
+      goal: string;
+      guess: string;
+      wordpack: wordPack;
+    }
   | {
       phase: "post-game";
       goal: string;
-    };
+      wordpack: wordPack;
+    }
+>;
 
 type Action =
   | {
@@ -18,10 +27,17 @@ type Action =
   | {
       type: "update-guess";
       newGuess: string;
+    }
+  | {
+      type: "load-wordpack";
+      wordpack: wordPack;
     };
 
 function getInitialState(): State {
-  return { phase: "pre-game" };
+  return {
+    phase: "pre-game",
+    wordpack: null,
+  };
 }
 
 function getRandomWord() {
@@ -34,7 +50,15 @@ function reducer(state: State, action: Action): State {
       if (state.phase === "in-game") {
         return state;
       }
-      return { phase: "in-game", goal: getRandomWord(), guess: "" };
+      if (state.wordpack === null) {
+        return state;
+      }
+      return {
+        phase: "in-game",
+        goal: getRandomWord(),
+        guess: "",
+        wordpack: state.wordpack,
+      };
     }
     case "update-guess": {
       if (state.phase !== "in-game") {
@@ -44,9 +68,19 @@ function reducer(state: State, action: Action): State {
         return {
           phase: "post-game",
           goal: state.goal,
+          wordpack: state.wordpack,
         };
       }
-      return { ...state, guess: action.newGuess };
+      return {
+        ...state,
+        guess: action.newGuess,
+      };
+    }
+    case "load-wordpack": {
+      return {
+        ...state,
+        wordpack: action.wordpack
+      };
     }
   }
   return state;
@@ -54,6 +88,22 @@ function reducer(state: State, action: Action): State {
 
 function App() {
   const [state, dispatch] = useReducer(reducer, null, getInitialState);
+  useEffect(() => {
+    fetch(
+      "https://gist.githubusercontent.com/dracos/dd0668f281e685bad51479e5acaadb93/raw/6bfa15d263d6d5b63840a8e5b64e04b382fdb079/valid-wordle-words.txt",
+    )
+      .then((response) => response.text())
+      .then((text) =>
+        dispatch({
+          type: "load-wordpack",
+          wordpack: text
+            .split("\n")
+            .map((word) => word.toUpperCase().trim())
+            .filter(Boolean),
+        }),
+      );
+  }, []);
+
   let content = null;
   switch (state.phase) {
     case "pre-game": {
